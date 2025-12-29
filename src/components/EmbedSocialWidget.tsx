486c6f15dfc70075ba9af0338d65dc4ee46f2e05
Priya Sharma <priyasharma2702@gmail.com>
2025-12-29 23:48:34 +0530

    removing moving footer and making one at bottom

---
+++ src/components/EmbedSocialWidget.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useEffect } from 'react';
+
+const EmbedSocialWidget = () => {
+	useEffect(() => {
+		(function (d, s, id) {
+			var js;
+			if (d.getElementById(id)) {
+				return;
+			}
+			js = d.createElement(s);
+			js.id = id;
+			js.src = 'https://embedsocial.com/cdn/ht.js';
+			d.getElementsByTagName('head')[0].appendChild(js);
+		})(document, 'script', 'EmbedSocialHashtagScript');
+	}, []);
+
+	return (
+		<div
+			className='embedsocial-hashtag'
+			data-ref='be6e570494b6eb6176c55bfbb26f377d148d730b'>
+			<a
+				className='feed-powered-by-es'
+				href='https://embedsocial.com/products/embedfeed/'
+				target='_blank'
+				title='Powered by EmbedSocial'>
+				Powered by EmbedSocial<span>→</span>
+			</a>
+		</div>
+	);
+};
+
+export default EmbedSocialWidget;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,38 +5,45 @@ import Link from 'next/link';
 
 const Footer = () => {
 	return (
-		<footer className='fixed bottom-0 w-full left-0 bg-white h-15 lg:h-20'>
-			<hr className='border-red-600' />
-			<div className='flex flex-col justify-center gap-1 items-center my-0.5 lg:my-2 py-0.5 lg:py-2 animate-[var(--moving-text)] text-sm lg:text-lg'>
-				<h2 className='font-extralight text-center text-unwrap overflow-hidden'>
-					Nanhe Khwab Charitable Trust is 80G approved, ensuring that all
-					donations are fully tax-deductible and compliant with government
-					regulations.
-				</h2>
-				<div className='flex gap-1 lg:gap-2 justify-center items-center'>
-					<div className='flex gap-1 lg:gap-3'>
-						<Link href='https://www.instagram.com/nanhe_khwab/'>
-							<Image
-								src={LinkedInSVG}
-								className=''
-								alt='LinkedIn Icon'
-								width={20}
-							/>
-						</Link>
-						<Link href='https://www.instagram.com/nanhe_khwab/'>
-							<Image
-								src={InstagramSVG}
-								className=''
-								alt='Instagram Icon'
-								width={20}
-							/>
-						</Link>
+		<footer className='my-2'>
+			<section className='w-full bg-amber-600/80 border-t border-t-red-100 flex flex-col justify-center items-center gap-2 py-2'>
+				<div className='flex flex-row justify-center gap-1 items-center my-0.5 lg:my-2 py-0.5 lg:py-2 text-sm lg:text-lg'>
+					<div className='flex flex-col'>
+						<p className='font-extralight text-center text-unwrap overflow-hidden'>
+							Nanhe Khwab Charitable Trust is a registered trust under Bombay
+							Public Trusts Act, 1950. We are a 12A registered society with
+							section 80(G) status of income tax act. Nanhe Khwab Charitable
+							Trust is 80G approved, ensuring that all donations are fully
+							tax-deductible and compliant with government regulations.
+						</p>
+						<p>contact@nanhekhwab@gmail.com</p>
 					</div>
-					<div>
-						<p>Site is made by Miss Sharma</p>
+					<div className='flex gap-1 lg:gap-2 justify-center items-center'>
+						<div className='flex gap-1 lg:gap-3'>
+							<Link href='https://www.instagram.com/nanhe_khwab/'>
+								<Image
+									src={LinkedInSVG}
+									className=''
+									alt='LinkedIn Icon'
+									width={20}
+								/>
+							</Link>
+							<Link href='https://www.instagram.com/nanhe_khwab/'>
+								<Image
+									src={InstagramSVG}
+									className=''
+									alt='Instagram Icon'
+									width={20}
+								/>
+							</Link>
+						</div>
 					</div>
 				</div>
-			</div>
+				<div>
+					NANHE KHWAB LET THEM DREAM • ALL RIGHTS RESERVED • CONCEPTUALISED &
+					DESIGNED BY PRIYA SHARMA
+				</div>
+			</section>
 		</footer>
 	);
 };
